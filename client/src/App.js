df4387c992276ff3fcb1687fb053e9a9ccf0eb6e
Unknown <michaelsingarayar@gmail.com>
2019-03-19 21:42:05 -0600

    countdownTimer

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
 import logo from "./logo.svg";
+import CountDownTest from "./components/timer";
 import "./App.css";
+
 
 class App extends Component {
   render() {
     return (
+      <>
       <div className="App">
         <div className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
@@ -14,6 +17,11 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
       </div>
+
+      <CountDownTest />;
+
+      </>
+
     );
   }
 }
