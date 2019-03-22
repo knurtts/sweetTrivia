d03780546b7e5edb8f221743f81965ca340b755e
Simon Hong <simon_hong1@yahoo.com>
2019-03-21 21:05:07 -0600

    Add lobby page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import QuizLoop from "./pages/QuizLoop/QuizLoop";
 import Answered from "./pages/QuizLoop/Answered";
+import Lobby from "./pages/Lobby/Lobby";
 import "./App.css";
 
 
@@ -13,6 +14,7 @@ class App extends Component {
           <Route exact path="/" component={QuizLoop} />
           <Route exact path="/quiz" component={QuizLoop} />
           <Route exact path="/answered" component={Answered} />
+          <Route exact path="/lobby" component={Lobby} />
         </div>
       </Router>
     )
