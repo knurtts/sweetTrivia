ad60e1c570c9806d33fb5d2dcc04e0fe8300c082
Mike Corey <mjcorey57@gmail.com>
2019-03-22 14:31:04 -0600

    Merge Files

--- client/src/App.js
+++ client/src/App.js
@@ -2,19 +2,44 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import QuizLoop from "./pages/QuizLoop/QuizLoop";
 import Answered from "./pages/QuizLoop/Answered";
+import firebase from './firebase';
+import Navigation from './Navigation';
 import "./App.css";
 
 class App extends Component {
+
+  state = {
+    authenticated: false,
+  };
+
+  componentDidMount() {
+    console.log(this.state.authenticated);
+    firebase.auth().onAuthStateChanged((authenticated) => {
+      authenticated
+        ? this.setState(() => ({
+            authenticated: true,
+          }))
+        : this.setState(() => ({
+            authenticated: false,
+          }));
+      console.log(this.state.authenticated);
+    });
+  }
+
+
   render() {
     return (
+      
       <Router>
         <div>
           <Route exact path="/" component={QuizLoop} />
           <Route exact path="/quiz" component={QuizLoop} />
           <Route exact path="/answered" component={Answered} />
+          <Navigation authenticated={this.state.authenticated} />;
         </div>
       </Router>
     )
+    
   }
 }
 
