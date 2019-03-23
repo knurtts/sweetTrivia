4ccf5ae98d54bf3a0a0e26348940bf3659682db9
Mike Corey <mjcorey57@gmail.com>
2019-03-23 10:59:33 -0600

    Firebase Auth

---
+++ client/src/Dashboard.js
@@ -0,0 +1,10 @@
+import React from 'react';
+//import { Container, Flex, Box, Heading } from 'rebass';
+
+const Dashboard = () => {
+  return (
+    <h1>Welcome</h1>
+  );
+};
+
+export default Dashboard;
