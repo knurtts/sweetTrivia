36c5ce6089cdff3c27cdcc7412b778cd1e3fdaa0
Mike Corey <mjcorey57@gmail.com>
2019-03-30 14:06:39 -0600

    Timer Changes

--- client/src/pages/Lobby/Lobby.js
+++ client/src/pages/Lobby/Lobby.js
@@ -5,55 +5,64 @@ import moment from 'moment';
 
 class Lobby extends Component {
     state = {
-      times: []
+      times: [],
+      timeCount: '',
     }
-  
+    timeFunction = (res) => {
+        const times = res.data;
+        this.setState({ times });
+        const startTime = times[0].startTime 
+
+      //Format the data returned form SQL in a MM/DD/YYY format with 24hr time
+       const dateFormat = moment.utc(startTime).format('L HH:mm:ss');
+
+       //convert string to date time
+       const newDate = new Date(dateFormat);
+
+       //convert date time to milliseconds
+       const countDownDate = newDate.getTime();
+        
+        //console.log(dateFormat);
+        //console.log(milliseconds);
+
+        const interval = setInterval(() => {
+
+          // Get todays date and time
+          const now = new Date().getTime();
+        
+          // Find the distance between now and the count down date. Both times are in milliseconds
+          const distance = countDownDate - now;
+        
+          // Time calculations for days, hours, minutes and seconds
+          const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+          const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+          const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+          const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        
+          // Display the result
+          //console.log(
+           //days + ":" + hours + ":" + minutes + ":" + seconds);
+           this.setState({
+               timeCount: days + ":" + hours + ":" + minutes + ":" + seconds
+           })
+        
+          // If the count down is finished, write some text 
+          if (distance < 0) {
+            clearInterval(interval);
+            console.log("EXPIRED");
+            this.setState({
+                timeCount:"Time Over"
+            })
+          }
+        }, 1000); 
+    }
     componentDidMount() {
       axios.get('/api/starttime')
         .then(res => {
-          
-          const times = res.data;
-          this.setState({ times });
-          const startTime = times[0].startTime 
-  
-        //Format the data returned form SQL in a MM/DD/YYY format with 24hr time
-         const dateFormat = moment.utc(startTime).format('L HH:mm:ss');
-  
-         //convert string to date time
-         const newDate = new Date(dateFormat);
-  
-         //convert date time to milliseconds
-         const countDownDate = newDate.getTime();
-          
-          //console.log(dateFormat);
-          //console.log(milliseconds);
-  
-          const interval = setInterval(function() {
-  
-            // Get todays date and time
-            const now = new Date().getTime();
-          
-            // Find the distance between now and the count down date. Both times are in milliseconds
-            const distance = countDownDate - now;
-          
-            // Time calculations for days, hours, minutes and seconds
-            const days = Math.floor(distance / (1000 * 60 * 60 * 24));
-            const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-            const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-            const seconds = Math.floor((distance % (1000 * 60)) / 1000);
-          
-            // Display the result
-            console.log(
-             days + "d " + hours + "h "
-            + minutes + "m " + seconds + "s ");
-          
-            // If the count down is finished, write some text 
-            if (distance < 0) {
-              clearInterval(interval);
-              console.log("EXPIRED");
-              
-            }
-          }, 1000);   
+          //this.timeFunction(res)
+            this.setState({
+                timer: this.timeFunction(res)
+            })
           
         })
         .catch(function (error) {
@@ -68,16 +77,12 @@ class Lobby extends Component {
                     <div className="row">
                         <div className="col s12">
     
-                            <h1 className="card-title" id="title">The game will begin in</h1>
-    
-                                        <div id="timer"><h5></h5></div>
-    
+                            <h1 className="card-title" id="title">The game will begin in <div id="timer">{this.state.timeCount}</div></h1>
                             
-                            
-                            <div className="card blue">
+                            <div className="">
                                 <div className="card-content white-text" id="playerCard">
                                     <div className="card-title"><h5>Total Players:</h5></div>
-                                    <div className="card-panel yellow darken-4">
+                                    <div className="">
                                         <div className="white-text">
                                         {/* Player Count from DB */}
                                             10
